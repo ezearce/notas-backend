18f5385319c0eb5b9d810b571de48ff5729339d0
ezearce <ezequiel.arce@hotmail.com>
2025-07-29 18:02:07 -0300

    initial commit

---
+++ src/main/java/com/ezequielarce/notas/NotasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ezequielarce.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasBackendApplication.class, args);
+	}
+
+}
