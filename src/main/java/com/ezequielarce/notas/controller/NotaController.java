c9100da2d0875f00ff447f68e01925112a8586e5
ezearce <ezequiel.arce@hotmail.com>
2025-08-02 19:42:05 -0300

    Update NotaController.java

--- src/main/java/com/ezequielarce/notas/controller/NotaController.java
+++ src/main/java/com/ezequielarce/notas/controller/NotaController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/notas")
+@CrossOrigin(origins = "http://localhost:5173")
 public class NotaController {
     private final NotaService notaService;
 
